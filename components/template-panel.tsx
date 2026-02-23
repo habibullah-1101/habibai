d252ac4ae9af6cfd61ee6ab458a18b367b9f42dc
Habib Hussaini <js7p6hdbw5@privaterelay.appleid.com>
2026-02-23 22:57:32 +0430

    Use onSelectTemplate in TemplatePanel button click

--- components/template-panel.tsx
+++ components/template-panel.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { TEMPLATES } from "@/lib/templates";
 
 export function TemplatePanel({
   onSelectTemplate,
 }: {
   onSelectTemplate: (templateId: string) => void;
 }) {
-  return <Button>Templates</Button>;
+  return (
+    <Button onClick={() => onSelectTemplate(TEMPLATES[0]?.prompt ?? "")}>
+      Templates
+    </Button>
+  );
 }
